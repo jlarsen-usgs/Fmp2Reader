1e81ff9f8b907a1c3544b239c8ebbb9f0bea4876
Joshua Larsen <jlarsen@usgs.gov>
2017-11-17 09:30:02 -0800

    Added an __init__.py for python packaging to install as a python global library

---
+++ fmp2reader/__init__.py
@@ -0,0 +1,5 @@
+from .farm_process import Fmp
+from .ArrayToShapeFile import create_array_from_mflist
+from .ArrayToShapeFile import create_shapefile_from_array
+from .ArrayToShapeFile import create_shapefile_from_transient_array
+from .ArrayToShapeFile import create_shapefile_from_array
