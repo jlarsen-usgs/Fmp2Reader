cbb08c2ac8ef960a08d130bbcbcfef9af31e5573
Joshua Larsen <jlarsen@usgs.gov>
2017-11-21 14:38:23 -0800

    Update read tree to set None dtype and None data

--- fmp2reader/farm_process.py
+++ fmp2reader/farm_process.py
@@ -331,6 +331,8 @@ class Fmp(dict):
                         data, skip = self.__internal_reader(ix, block_data, key)
                         if data is not None:
                             dtype = "{} {}".format(t[1].lower(), t[2].lower())
+                        else:
+                            dtype = None
 
                     elif t[1].lower() == "open/close":
                         data = self.__array_reader(t[-1])
